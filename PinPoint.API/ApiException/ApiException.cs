4255c42de5bed71e9c6b906eec16ba59e1abe945
Hayrettin <hayrettin.gov@gmail.com>
2023-02-14 14:01:03 +0300

    auth

---
+++ PinPoint.API/ApiException/ApiException.cs
@@ -0,0 +1,15 @@
+﻿namespace PinPoint.API.ApiException
+{
+    public class ApiException : Exception
+    {
+        internal ApiException(string businessMessage)
+            : base(businessMessage)
+        {
+        }
+
+        internal ApiException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+    }
+}
---
+++ PinPoint.Infrastructure/MapperService/Models/User/DeleteUserDTO.cs
@@ -0,0 +1,9 @@
+﻿using PinPoint.Data.Domain.Base;
+
+namespace PinPoint.Infrastructure.MapperService.Models.User
+{
+    public class DeleteUserDTO : AdditionalDomain
+    {
+        public Guid user_id { get; set; }
+    }
+}
